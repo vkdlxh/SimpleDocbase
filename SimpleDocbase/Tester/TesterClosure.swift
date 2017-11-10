c1e64092ab90aaa8ad453b3fc39dfb3e01fdf244
vkdlxh <tkdwnszoq@naver.com>
2017-11-10 15:07:29 +0900

    create defalut files

---
+++ SimpleDocbase/Tester/TesterClosure.swift
@@ -0,0 +1,14 @@
+//
+//  TesterClosure.swift
+//  SimpleDocbase
+//
+//  Created by jeonsangjun on 2017/11/10.
+//  Copyright © 2017年 archive-asia. All rights reserved.
+//
+
+import Foundation
+
+class TesterClosure {
+    
+    
+}
---
+++ SimpleDocbase/Tester/TesterViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TesterViewController.swift
+//  SimpleDocbase
+//
+//  Created by jeonsangjun on 2017/11/10.
+//  Copyright © 2017年 archive-asia. All rights reserved.
+//
+
+import UIKit
+
+class TesterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
