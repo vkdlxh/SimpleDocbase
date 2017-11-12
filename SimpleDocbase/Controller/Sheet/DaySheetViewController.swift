b0fec1e00446147078ce7891b55d3bb3d6becb29
jaeeun <leejaeeun@archive-asia.co.jp>
2017-11-13 00:34:32 +0900

    勤怠管理で使う部品追加 画面を少し実装

---
+++ SimpleDocbase/Controller/Sheet/DaySheetViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DaySheetViewController.swift
+//  SimpleDocbase
+//
+//  Created by jaeeun on 2017/11/11.
+//  Copyright © 2017年 archive-asia. All rights reserved.
+//
+
+import UIKit
+
+final class DaySheetViewController : UIViewController {
+    
+    // MARK: Properties
+    var sheetItem : WorkSheetItem?
+}
