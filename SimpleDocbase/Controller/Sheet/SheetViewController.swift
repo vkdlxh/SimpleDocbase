be7334c277502a1c45cae31df6c46b95445e113e
jaeeun <leejaeeun@archive-asia.co.jp>
2017-11-13 00:03:41 +0900

    表示のための部品作成、 勤怠管理画面の枠をすこ実装。

--- SimpleDocbase/Controller/Sheet/SheetViewController.swift
+++ SimpleDocbase/Controller/Sheet/SheetViewController.swift
@@ -68,8 +68,12 @@ final class SheetViewController : UIViewController {
     // MARK: Private Methods
     private func loadTestData() {
         for i in 0..<10 {
-            var work_sheet = WorkSheet(title: "タイトル_\(i)")
-            
+            guard let year_month = Date.createDate(year: 2017, month: i+1) else {
+                continue
+            }
+            var work_sheet = WorkSheet(date:year_month)
+            work_sheet.workDaySum = 10 + Int(arc4random()%10)
+            work_sheet.workTimeSum = Double(120) + Double(arc4random()%20)
             for j in 0..<31 {
                 let work_sheet_item = WorkSheetItem(year: "2017", month: "\(i)", day: "\(j)")
                 work_sheet.items.append(work_sheet_item)
@@ -84,7 +88,7 @@ final class SheetViewController : UIViewController {
 extension SheetViewController : UITableViewDelegate {
     
     internal func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 44.0;
+        return 100.0;
     }
     
     internal func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -101,10 +105,11 @@ extension SheetViewController : UITableViewDataSource {
     }
     
     internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = UITableViewCell(style: .default, reuseIdentifier: "WorkSheetCell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WorkSheetCell") as! WorkSheetCell
         
         let workSheet = workSheets[indexPath.row]
-        cell.textLabel?.text = workSheet.title
+        cell.settingCell(workSheet)
+        
         return cell
     }
 
--- SimpleDocbase/Extension/Date+WorkSheet.swift
+++ SimpleDocbase/Extension/Date+WorkSheet.swift
@@ -16,7 +16,7 @@ import UIKit
 extension Date  {
     
     func defaultCalendar() -> Calendar {
-        var calendar = Calendar.current
+        let calendar = Calendar.current
         //calendar.locale = Locale(identifier: "ja_JP")
         //calendar.timeZone = TimeZone(identifier: "Asia/Tokyo")!
         
@@ -81,5 +81,30 @@ extension Date  {
         default:
             return false
         }
+    }
+    
+    func yearMonthString() -> String {
+        
+        let dateFormater = DateFormatter()
+        dateFormater.locale = Locale(identifier: "ja_JP")
+        //dateFormater.dateFormat = "yyyy/MM/dd HH:mm:ss"
+        dateFormater.dateFormat = "yyyy/MM"
+        let dateString = dateFormater.string(from: self)
+        print(dateString)
+        
+        return dateString
+    }
+    
+    static func createDate(year: Int, month: Int) -> Date? {
+        
+        let conv_month = NSString(format: "%02d", month)
+        let dateStr = "\(year)\(conv_month)"
+        
+        let dateFormater = DateFormatter()
+        dateFormater.locale = Locale(identifier: "ja_JP")
+        dateFormater.dateFormat = "yyyyMM"
+        let date = dateFormater.date(from: dateStr)
+        return date
+        
     }
 }
