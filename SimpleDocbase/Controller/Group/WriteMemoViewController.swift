4b6112e588eb0f136e846d71aba5f0365fbcaf4c
vkdlxh <tkdwnszoq@naver.com>
2017-12-14 18:20:45 +0900

    issue#63対応

--- SimpleDocbase/Controller/Group/WriteMemoViewController.swift
+++ SimpleDocbase/Controller/Group/WriteMemoViewController.swift
@@ -20,6 +20,7 @@ class WriteMemoViewController: UIViewController {
     var group: Group?
     var checkWriteSuccess = false
     let tagValue = "iPhoneから投稿"
+    let bodyPlaceHolder = "本文を入力してください。"
     
     // MARK: IBOutlets
     @IBOutlet weak var titleTextField: UITextField!
@@ -64,6 +65,11 @@ class WriteMemoViewController: UIViewController {
     // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        bodyTextView.delegate = self
+        bodyTextView.text = bodyPlaceHolder
+        bodyTextView.textColor = UIColor.lightGray
+        bodyTextView.selectedTextRange = bodyTextView.textRange(from: bodyTextView.beginningOfDocument, to: bodyTextView.beginningOfDocument)
         
         if let groupName = group?.name {
             navigationItem.title = groupName
@@ -129,5 +135,37 @@ class WriteMemoViewController: UIViewController {
         }
         ac.addAction(okAction)
         self.present(ac, animated: true, completion: nil)
+    }
+}
+
+extension WriteMemoViewController: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let currentText = textView.text as NSString?
+        let updatedText = currentText?.replacingCharacters(in: range, with: text)
+
+        if (updatedText?.isEmpty)!  {
+            
+            textView.text = "Placeholder"
+            textView.textColor = UIColor.lightGray
+            
+            textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
+            
+            return false
+        }
+
+        else if textView.textColor == UIColor.lightGray && !text.isEmpty {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+        
+        return true
+    }
+    
+    func textViewDidChangeSelection(_ textView: UITextView) {
+        if self.view.window != nil {
+            if textView.textColor == UIColor.lightGray {
+                textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
+            }
+        }
     }
 }
